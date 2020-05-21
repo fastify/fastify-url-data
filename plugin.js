4fed5eacf162362a74141539d38918440443afa2
Li Chen <lichen@youzan.com>
2020-05-21 16:45:31 +0800

    fix: request.req is deprecated, use request.raw instead

--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@ function plugin (fastify, options, next) {
   fastify.decorateRequest('urlData', function (key) {
     const scheme = (this.headers[':scheme'] ? this.headers[':scheme'] + ':' : '') + '//'
     const host = this.headers[':authority'] || this.headers.host
-    const path = this.headers[':path'] || this.req.url
+    const path = this.headers[':path'] || this.raw.url
     const urlData = urijs.parse(scheme + host + path)
     if (key) return urlData[key]
     return urlData
