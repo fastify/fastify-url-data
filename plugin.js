5070d821f880bf7c717ed2722160e9fd0446b8d6
Andrii Kostenko <andrii@short.io>
2021-07-09 17:05:11 +0300

    add semicolon

--- plugin.js
+++ plugin.js
@@ -5,7 +5,7 @@ const urijs = require('uri-js')
 
 function plugin (fastify, options, next) {
   fastify.decorateRequest('urlData', function (key) {
-    const scheme = (this.headers[':scheme'] ? this.headers[':scheme'] + ':' : this.protocol) + '//'
+    const scheme = (this.headers[':scheme'] ? this.headers[':scheme'] + ':' : this.protocol + ':') + '//'
     const host = this.headers[':authority'] || this.headers.host
     const path = this.headers[':path'] || this.raw.url
     const urlData = urijs.parse(scheme + host + path)
