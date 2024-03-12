466ae936fccc2a8bc2b25086fdccdc4eb2145233
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2024-03-12 14:22:28 +0100

    replace fast-uri url parser with URL contructor

--- types/fastify-url-data.test-d.ts
+++ types/fastify-url-data.test-d.ts
@@ -7,14 +7,13 @@ const server = fastify();
 server.register(urlData)
 
 server.get('/data', (req, reply) => {
-  console.log(req.urlData)
-  expectType<string|undefined>(req.urlData().path);
-  expectType<string|undefined>(req.urlData().host);
-  expectType<string|number|undefined>(req.urlData().port);
-  expectType<string|undefined>(req.urlData().query);
+  expectType<string|''>(req.urlData().pathname);
+  expectType<string|''>(req.urlData().hostname);
+  expectType<string|''>(req.urlData().port);
+  expectType<string|''>(req.urlData().search);
 
-  expectType<string|undefined>(req.urlData('path'));
-  expectType<string|number|undefined>(req.urlData('port'));
+  expectType<string|''>(req.urlData('pathname'));
+  expectType<string|''>(req.urlData('port'));
 
   reply.send({msg: 'ok'})
 })
