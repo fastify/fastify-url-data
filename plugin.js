c8dd240701632a0c23e691610193e13c8b10af4c
Gürgün Dayıoğlu <hey@gurgun.day>
2024-07-13 21:52:47 +0300

    fix port

--- plugin.js
+++ plugin.js
@@ -1,14 +1,15 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const fastUri = require('fast-uri')
+const { parse } = require('fast-uri')
 
 function fastifyUrlData (fastify, options, next) {
   fastify.decorateRequest('urlData', function (key) {
     const scheme = this.headers[':scheme'] ? this.headers[':scheme'] : this.protocol
     const host = this.hostname
+    const port = this.port
     const path = this.headers[':path'] || this.raw.url
-    const urlData = fastUri.parse(scheme + '://' + host + path)
+    const urlData = parse(`${scheme}://${host}:${port}${path}`)
     if (key) return urlData[key]
     return urlData
   })
