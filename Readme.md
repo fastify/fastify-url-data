ba1ba50ef1f9d38d7d8b3878d3694edb15d94d04
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 10:45:19 +0000

    build(deps-dev): replace standard with neostandard

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 
 [![CI](https://github.com/fastify/fastify-url-data/actions/workflows/ci.yml/badge.svg?branch=master)](https://github.com/fastify/fastify-url-data/actions/workflows/ci.yml)
 [![NPM version](https://img.shields.io/npm/v/@fastify/url-data.svg?style=flat)](https://www.npmjs.com/package/@fastify/url-data)
-[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://standardjs.com/)
+[![neostandard javascript style](https://img.shields.io/badge/code_style-neostandard-brightgreen?style=flat)](https://github.com/neostandard/neostandard)
 
 A plugin for [Fastify](https://fastify.dev/) that adds support for getting raw
 URL information from the request.
--- types/fastify-url-data.test-d.ts
+++ types/fastify-url-data.test-d.ts
@@ -2,21 +2,21 @@ import fastify from 'fastify'
 import { expectType } from 'tsd'
 import urlData from '..'
 
-const server = fastify();
+const server = fastify()
 
 server.register(urlData)
 
 server.get('/data', (req, reply) => {
   console.log(req.urlData)
-  expectType<string|undefined>(req.urlData().path);
-  expectType<string|undefined>(req.urlData().host);
-  expectType<string|number|undefined>(req.urlData().port);
-  expectType<string|undefined>(req.urlData().query);
+  expectType<string | undefined>(req.urlData().path)
+  expectType<string | undefined>(req.urlData().host)
+  expectType<string | number | undefined>(req.urlData().port)
+  expectType<string | undefined>(req.urlData().query)
 
-  expectType<string|undefined>(req.urlData('path'));
-  expectType<string|number|undefined>(req.urlData('port'));
+  expectType<string | undefined>(req.urlData('path'))
+  expectType<string | number | undefined>(req.urlData('port'))
 
-  reply.send({msg: 'ok'})
+  reply.send({ msg: 'ok' })
 })
 
 server.listen({ port: 3030 })
