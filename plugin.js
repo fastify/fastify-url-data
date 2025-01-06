b8e657cb5a4e5e2cabdc7b451412da784e583d63
Frazer Smith <frazer.dev@icloud.com>
2025-01-06 18:01:12 +0000

    refactor(plugin): prefix unused param with underscore (#161)
    Signed-off-by: Frazer Smith <frazer.dev@icloud.com>

--- plugin.js
+++ plugin.js
@@ -3,7 +3,7 @@
 const fp = require('fastify-plugin')
 const { parse } = require('fast-uri')
 
-function fastifyUrlData (fastify, options, next) {
+function fastifyUrlData (fastify, _options, next) {
   fastify.decorateRequest('urlData', function (key) {
     const scheme = this.headers[':scheme'] ? this.headers[':scheme'] : this.protocol
     const host = this.hostname
